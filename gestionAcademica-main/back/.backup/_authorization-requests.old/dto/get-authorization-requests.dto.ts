02c03e3bc782b85cd019f6d208dcb7a49b1711a7
xKonuC <ignacio.lopez.1907@gmail.com>
2025-11-27 02:23:37 -0300

    ACTUALIZADO A EMPRESA
    actualizado a empresa / proyecto

--- gestionAcademica-main/back/.backup/_authorization-requests.old/dto/get-authorization-requests.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsOptional, IsString } from 'class-validator';
-
-export class ListAuthorizationRequestsDto {
-  @IsOptional() @IsString() q?: string;
-  @IsOptional() @IsString() institution?: string;
-  @IsOptional() @IsString() status?: string;
-}
--- gestionAcademica-main/back/.backup/_carta.old/carta.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CartaController } from './carta.controller';
-import { CartaService } from './carta.service';
-
-@Module({
-  controllers: [CartaController],
-  providers: [CartaService],
-})
-export class CartaModule {}
